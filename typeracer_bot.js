f9c95e2894c28bd5024716830047f8e4d52e73e7
Josh Bishop <jsh.bshp@outlook.com>
2019-05-11 13:29:50 -0400

    update

--- typeracer_bot.js
+++ typeracer_bot.js
@@ -3,7 +3,7 @@ var good = new Array();
 var wordsCount = 0
 
 for (var i = 0; i < spans.length; i++) {
-	if(spans[i].className == "lblUsername") {
+	if(spans[i].className == "lblUsername" && spans[i + 1].className != "lblUsername") {
 		for (var i; i < spans.length; i++) {
 			good.push(spans[i]);
 			if(spans[i].className == "gwt-InlineLabel") {
@@ -22,10 +22,16 @@ var words = fullString.split(" ");
 
 
 txts[0].value = words[wordsCount];
+txts[0].focus();
 document.body.onkeyup = function(e) {
 	if(e.keyCode == 32) {
 		wordsCount = wordsCount + 1;
-		console.log(words[wordsCount]);
 		txts[0].value = words[wordsCount];
+		txts[0].focus();
+		
+		var evt = new KeyboardEvent('keyup', {'keyCode':32, 'which':32});
+		setInterval(function() {
+			document.dispatchEvent (evt);
+		}, 100);
 	}
 }
